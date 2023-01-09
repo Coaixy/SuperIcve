6534f404f6cc0161dec4c4237cd1b60d461ce204
Coaixy <coaixy@outlook.com>
2023-01-09 10:53:37 +0800

    完善APIS.kt

--- src/main/kotlin/Data/Apis.kt
+++ src/main/kotlin/Data/Apis.kt
@@ -4,4 +4,20 @@ package Data
  * 存储API URL
  */
 object Apis {
+    //个人信息
+    const val index = "https://zjy2.icve.com.cn/api/student/Studio/index"
+    //课程列表
+    const val learningCourseList = "https://zjy2.icve.com.cn/api/student/learning/getLearnningCourseList"
+    //进度
+    const val processList = "https://zjy2.icve.com.cn/api/study/process/getProcessList"
+    //课程列表
+    const val stuStudyClassList = "https://zjy2.icve.com.cn/api/common/courseLoad/getStuStudyClassList"
+    //Topic
+    const val topicByModuleId = "https://zjy2.icve.com.cn/api/study/process/getTopicByModuleId"
+    //数据上传
+    const val stuProcessCellLog = "https://zjy2.icve.com.cn/api/common/Directory/stuProcessCellLog"
+    //获取CellID和LogCellID
+    const val viewDirectory = "https://zjy2.icve.com.cn/api/common/Directory/viewDirectory"
+    //获取Cell Info
+    const val cellByTopicId = "https://zjy2.icve.com.cn/api/study/process/getCellByTopicId"
 }
